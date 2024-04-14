2337339c79b99684d808a783a301c0e5bdcc6b6e
YOGENDRA KUMAR <yk70840@gmail.com>
2024-04-14 19:48:39 +0530

    Update index.py

--- index.py
+++ index.py
@@ -6,23 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    return """
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Image Prediction</title>
-    </head>
-    <body>
-        <h1>Upload Image</h1>
-        <form action="/predict" method="post" enctype="multipart/form-data">
-            <input type="file" name="image">
-            <button type="submit">Predict</button>
-        </form>
-    </body>
-    </html>
-    """
+    return {"message":"Hello"}
 
 @app.route("/predict", methods=["POST"])
 def process_image():
